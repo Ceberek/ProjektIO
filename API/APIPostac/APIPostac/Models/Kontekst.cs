0d022c48784e6265cd193328495fc2627bb61667
Maksiolo20 <maksiolo2000@interia.pl>
2021-02-08 19:57:16 +0100

    dodanie konfiguracji połączenia z bazą

--- API/APIPostac/APIPostac/Models/Kontekst.cs
+++ API/APIPostac/APIPostac/Models/Kontekst.cs
@@ -15,7 +15,11 @@ namespace APIPostac.Models
         {
             base.OnModelCreating(builder);
         }
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=API;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
     }
 
 }
