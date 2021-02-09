29e67804552a56e46da1289f3f5910bbba620bf8
Maksiolo20 <maksiolo2000@interia.pl>
2021-02-09 20:05:48 +0100

    Stworzenie strony z formularzem KP
    (KP=karta postaci)

---
+++ StronaKartaPostaci/index.php
@@ -0,0 +1,22 @@
+
+<html>
+<head>
+
+</head>
+
+<body style="background-color: #ffffff;">
+<div style="text-align: center">
+
+
+
+
+	<form class="formularz-tworzeniepostaci" method="POST" action="tworzenie-postaci.php" style="text-align:center; padding-top:2%;">
+		
+	
+	</form>
+</div>
+
+
+</body>
+</html>
+
