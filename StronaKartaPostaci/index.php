55c9c6b47fa5536c5c2366d9c1fba94bae25e1cf
Maksiolo20 <maksiolo2000@interia.pl>
2021-02-09 20:12:25 +0100

    Dodanie listy atrybutów

--- StronaKartaPostaci/index.php
+++ StronaKartaPostaci/index.php
@@ -12,6 +12,15 @@
 
 	<form class="formularz-tworzeniepostaci" method="POST" action="tworzenie-postaci.php" style="text-align:center; padding-top:2%;">
 		
+		<b>Atrybuty:</b>
+		<ul style="list-style-type:none;">
+		<li><input class="pass" type="number" align="center" name="sila" placeholder="Siła"></li>
+		<li><input class="pass" type="number" align="center" name="zrecznosc" placeholder="Zręczność"></li>
+		<li><input class="pass" type="number" align="center" name="inteligencja" placeholder="Inteligencja"></li>
+		<li><input class="pass" type="number" align="center" name="charyzma" placeholder="Charyzma"></li>
+		</ul>
+		<br>
+
 	<input type="submit" value="Utwórz Postać" name="rejestruj" style="margin-top: 8%;" class="submit" align="center">
 	</form>
 </div>
