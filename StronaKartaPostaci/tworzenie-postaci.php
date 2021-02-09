8a1f2b9008522b8cf38d629ec713fb89a7ced90d
Maksiolo20 <maksiolo2000@interia.pl>
2021-02-09 22:36:03 +0100

    Stworzenie zmiennych odpowiedzialych za umiejętności postaci

--- StronaKartaPostaci/tworzenie-postaci.php
+++ StronaKartaPostaci/tworzenie-postaci.php
@@ -1,7 +1,26 @@
 <?php
 session_start();
 $con = mysqli_connect('localhost', 'kowalczykmaciej', 'Qweasd123098','Postacie' ) or die('Brak połączenia z serwerem MySQL.');
+$sila = $_POST['sila'];
+$zrecznosc = $_POST['zrecznosc'];
+$inteligencja = $_POST['inteligencja'];
+$charyzma = $_POST['charyzma'];
 
+$wytrzymalosc = $_POST['wytrzymalosc'];
+$cios = $_POST['cios'];
+$lucznictwo = $_POST['lucznictwo'];
+
+$szermierka = $_POST['szermierka'];
+$jezdziectwo = $_POST['jezdziectwo'];
+$opatrywanie = $_POST['opatrywanie'];
+
+$taktyka = $_POST['taktyka'];
+$nawigacja = $_POST['nawigacja'];
+$inzynieria = $_POST['inzynieria'];
+
+$perswazja = $_POST['perswazja'];
+$przywodztwo = $_POST['przywodztwo'];
+$handel = $_POST['handel'];
 
 exit();
 ?>
