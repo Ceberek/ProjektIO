2642c496a934b5d6b0f3d95fe54f18fbc8ef042c
Maksiolo20 <maksiolo2000@interia.pl>
2021-02-09 20:13:58 +0100

    Dodanie list umiejętności
    z podziałem na zależne od odpowiednich artybutów. Jeszcze nie zaimplementowano zależności od atrybutów

--- StronaKartaPostaci/index.php
+++ StronaKartaPostaci/index.php
@@ -21,6 +21,37 @@
 		</ul>
 		<br>
 
+		<b>Umiejętności:</b><br><br>
+		Związane z siłą:
+		<ul style="list-style-type:none;">
+		<li><input class="pass" type="number" align="center" name="wytrzymalosc" placeholder="Wytrzymałość"></li>
+		<li><input class="pass" type="number" align="center" name="cios" placeholder="Siła ciosu"></li>
+		<li><input class="pass" type="number" align="center" name="lucznictwo" placeholder="Łucznictwo"></li>
+		</ul>
+
+		<br>
+		Związane ze zręcznością:
+		<ul style="list-style-type:none;">
+		<li><input class="pass" type="number" align="center" name="szermierka" placeholder="Szermierka"></li>
+		<li><input class="pass" type="number" align="center" name="jezdziectwo" placeholder="Jeździectwo"></li>
+		<li><input class="pass" type="number" align="center" name="opatrywanie" placeholder="Opatrywanie ran"></li>
+		</ul>
+
+		<br>
+		Związane z inteligencją:
+		<ul style="list-style-type:none;">
+		<li><input class="pass" type="number" align="center" name="taktyka" placeholder="Taktyka"></li>
+		<li><input class="pass" type="number" align="center" name="nawigacja" placeholder="Nawigacja"></li>
+		<li><input class="pass" type="number" align="center" name="inzynieria" placeholder="Inżynieria"></li>
+		</ul>
+
+		<br>
+		Związane z charyzmą:
+		<ul style="list-style-type:none;">
+		<li><input class="pass" type="number" align="center" name="perswazja" placeholder="Perswazja"></li>
+		<li><input class="pass" type="number" align="center" name="przywodztwo" placeholder="Przywództwo"></li>
+		<li><input class="pass" type="number" align="center" name="handel" placeholder="Handel"></li>
+		</ul>
 	<input type="submit" value="Utwórz Postać" name="rejestruj" style="margin-top: 8%;" class="submit" align="center">
 	</form>
 </div>
