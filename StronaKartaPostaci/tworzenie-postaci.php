c5be740c9831c8ca4e3ac587833153c94905be07
Maksiolo20 <maksiolo2000@interia.pl>
2021-02-09 22:41:29 +0100

    Wpisanie wartości do kolumn bazy danych

--- StronaKartaPostaci/tworzenie-postaci.php
+++ StronaKartaPostaci/tworzenie-postaci.php
@@ -22,5 +22,12 @@ $perswazja = $_POST['perswazja'];
 $przywodztwo = $_POST['przywodztwo'];
 $handel = $_POST['handel'];
 
+    $con->query("INSERT INTO Postac(Sila,Zrecznosc,Inteligencja,Charyzma, Wytrzymalosc, Cios, Lucznictwo,
+		Szermierka, Jezdziectwo, Opatrywanie, Taktyka, Nawigacja, Inzynieria, Perswazja, Przywodztwo, Handel)
+        VALUES( '$sila', '$zrecznosc', '$inteligencja', '$charyzma','$wytrzymalosc', '$cios', '$lucznictwo', '$szermierka', '$jezdziectwo', '$opatrywanie',
+		'$taktyka', '$nawigacja', '$inzynieria', '$perswazja', '$przywodztwo', '$handel')");
+    $con->close();
+
+
 exit();
 ?>
