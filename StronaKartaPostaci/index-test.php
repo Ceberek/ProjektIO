c99c82796408ab2b56a5fed1c87a1c77e95804c9
Maksiolo20 <maksiolo2000@interia.pl>
2021-02-09 20:06:52 +0100

    Stworzenie przycisku "stwórz postać"
    rozpoczynającego proces zapisania postaci w bazie danych

---
+++ StronaKartaPostaci/index-test.php
@@ -0,0 +1,22 @@
+
+<html>
+<head>
+
+</head>
+
+<body style="background-color: #ffffff;">
+<div style="text-align: center">
+
+
+
+
+	<form class="formularz-tworzeniepostaci" method="POST" action="tworzenie-postaci.php" style="text-align:center; padding-top:2%;">
+		
+	<input type="submit" value="Utwórz Postać" name="rejestruj" style="margin-top: 8%;" class="submit" align="center">
+	</form>
+</div>
+
+
+</body>
+</html>
+
