aeb352c917ed0a3a220daa07f494394963c83aaf
Maksiolo20 <maksiolo2000@interia.pl>
2021-02-10 13:28:31 +0100

    Ograniczenie poziomów do liczb nieujemnych

--- StronaKartaPostaci/index.php
+++ StronaKartaPostaci/index.php
@@ -14,43 +14,43 @@
 		
 		<b>Atrybuty:</b>
 		<ul style="list-style-type:none;">
-		<li><input class="pass" type="number" align="center" name="sila" placeholder="Siła"></li>
-		<li><input class="pass" type="number" align="center" name="zrecznosc" placeholder="Zręczność"></li>
-		<li><input class="pass" type="number" align="center" name="inteligencja" placeholder="Inteligencja"></li>
-		<li><input class="pass" type="number" align="center" name="charyzma" placeholder="Charyzma"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="sila" placeholder="Siła"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="zrecznosc" placeholder="Zręczność"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="inteligencja" placeholder="Inteligencja"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="charyzma" placeholder="Charyzma"></li>
 		</ul>
 		<br>
 
 		<b>Umiejętności:</b><br><br>
 		Związane z siłą:
 		<ul style="list-style-type:none;">
-		<li><input class="pass" type="number" align="center" name="wytrzymalosc" placeholder="Wytrzymałość"></li>
-		<li><input class="pass" type="number" align="center" name="cios" placeholder="Siła ciosu"></li>
-		<li><input class="pass" type="number" align="center" name="lucznictwo" placeholder="Łucznictwo"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="wytrzymalosc" placeholder="Wytrzymałość"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="cios" placeholder="Siła ciosu"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="lucznictwo" placeholder="Łucznictwo"></li>
 		</ul>
 
 		<br>
 		Związane ze zręcznością:
 		<ul style="list-style-type:none;">
-		<li><input class="pass" type="number" align="center" name="szermierka" placeholder="Szermierka"></li>
-		<li><input class="pass" type="number" align="center" name="jezdziectwo" placeholder="Jeździectwo"></li>
-		<li><input class="pass" type="number" align="center" name="opatrywanie" placeholder="Opatrywanie ran"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="szermierka" placeholder="Szermierka"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="jezdziectwo" placeholder="Jeździectwo"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="opatrywanie" placeholder="Opatrywanie ran"></li>
 		</ul>
 
 		<br>
 		Związane z inteligencją:
 		<ul style="list-style-type:none;">
-		<li><input class="pass" type="number" align="center" name="taktyka" placeholder="Taktyka"></li>
-		<li><input class="pass" type="number" align="center" name="nawigacja" placeholder="Nawigacja"></li>
-		<li><input class="pass" type="number" align="center" name="inzynieria" placeholder="Inżynieria"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="taktyka" placeholder="Taktyka"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="nawigacja" placeholder="Nawigacja"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="inzynieria" placeholder="Inżynieria"></li>
 		</ul>
 
 		<br>
 		Związane z charyzmą:
 		<ul style="list-style-type:none;">
-		<li><input class="pass" type="number" align="center" name="perswazja" placeholder="Perswazja"></li>
-		<li><input class="pass" type="number" align="center" name="przywodztwo" placeholder="Przywództwo"></li>
-		<li><input class="pass" type="number" align="center" name="handel" placeholder="Handel"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="perswazja" placeholder="Perswazja"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="przywodztwo" placeholder="Przywództwo"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="handel" placeholder="Handel"></li>
 		</ul>
 
 	<input type="submit" value="Utwórz Postać" name="rejestruj" style="margin-top: 8%;" class="submit" align="center">
