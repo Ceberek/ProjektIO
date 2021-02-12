70be8bddeb4ed3b92e76ce9e92b1bb23f027d24a
Maksiolo20 <maksiolo2000@interia.pl>
2021-02-12 18:44:03 +0100

    Utworzenie funkcji odpowiadających za zliczanie ilości punktów umiejętności

--- StronaKartaPostaci/index.php
+++ StronaKartaPostaci/index.php
@@ -15,7 +15,58 @@ var z = document.getElementById("zre").value;
 var i = document.getElementById("int").value;
 var c = document.getElementById("cha").value;
 iloscPkt=20-s-z-i-c;
-document.getElementById("pkt").innerHTML="Siła: "+s+" Zręczność: "+z+" Inteligencja: "+i+" Charyzma: "+c+" Ogółem: "+iloscPkt;
+document.getElementById("pkt").innerHTML="Siła: "+s+" Zręczność: "+z+" Inteligencja: "+i+" Charyzma: "+c+" Nieprzydzielone: "+iloscPkt;
+}
+
+var iloscPktUmiej=10;
+
+function max10sil()
+{
+var wytrz = document.getElementById("wy").value;
+var cios = document.getElementById("ci").value;
+var luczn = document.getElementById("lu").value;
+
+var sumSil=Math.abs(0-wytrz-cios-luczn);
+
+document.getElementById("pktUmiejSil").innerHTML=sumSil;
+}
+
+function max10zre()
+{
+
+var szerm = document.getElementById("sz").value;
+var jezdz = document.getElementById("je").value;
+var opatr = document.getElementById("op").value;
+
+var sumZre=Math.abs(0-szerm-jezdz-opatr);
+
+document.getElementById("pktUmiejZre").innerHTML=sumZre;
+}
+function max10int()
+{
+
+var taktyk = document.getElementById("ta").value;
+var naiwg = document.getElementById("na").value;
+var inzyn = document.getElementById("in").value;
+
+var sumInt = taktyk+naiwg+inzyn;
+// iloscPktUmiej=10-szerm-jezdz-opatr;
+}
+function max10cha()
+{
+
+var pers = document.getElementById("pe").value;
+var przyw = document.getElementById("pr").value;
+var hand = document.getElementById("ha").value;
+
+var sumCha = pers+przyw+hand;
+// iloscPktUmiej=10-szerm-jezdz-opatr;
+}
+function sumWszys()
+{
+var sumaWszystkich=sumSil+sumZre+sumInt+sumCha;
+iloscPktUmiej=iloscPktUmiej-sumaWszystkich;
+document.getElementById("pktUmiej").innerHTML=iloscPktUmiej;
 }
 </script>
 
@@ -59,40 +110,42 @@ document.getElementById("pkt").innerHTML="Siła: "+s+" Zręczność: "+z+" Intel
 
 <section class="umiejki">
 		<h2><b>Umiejętności:</b></h2>
-		Masz do wydania 10pkt
+		Masz do wydania <div id="pktUmiej"></div>
 		<br>
 		<br>
 		
-		<p>Związane z siłą:</p>
-		<ul style="list-style-type:none;">
-		<li><input class="pass" type="number" min="0" align="center" name="wytrzymalosc" placeholder="Wytrzymałość"></li>
-		<li><input class="pass" type="number" min="0" align="center" name="cios" placeholder="Siła ciosu"></li>
-		<li><input class="pass" type="number" min="0" align="center" name="lucznictwo" placeholder="Łucznictwo"></li>
+		<p>Związane z siłą: <div id="pktUmiejSil"></div></p>
+		<ul class="lista" onchange="max10sil()" style="list-style-type:none;">
+		<li><input class="pass" type="number" min="0" align="center" name="wytrzymalosc" placeholder="Wytrzymałość" id="wy"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="cios" placeholder="Siła ciosu" id="ci"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="lucznictwo" placeholder="Łucznictwo" id="lu"></li>
+		</ul>
+		
+		<br>
+		<p>Związane ze zręcznością: <div id="pktUmiejZre"></div></p>
+		<ul class="lista" onchange="max10zre()" style="list-style-type:none;">
+		<li><input class="pass" type="number" min="0" align="center" name="szermierka" placeholder="Szermierka" id="sz"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="jezdziectwo" placeholder="Jeździectwo" id="je"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="opatrywanie" placeholder="Opatrywanie ran" id="op"></li>
 		</ul>
 
 		<br>
-		<p>Związane ze zręcznością:</p>
-		<ul style="list-style-type:none;">
-		<li><input class="pass" type="number" min="0" align="center" name="szermierka" placeholder="Szermierka"></li>
-		<li><input class="pass" type="number" min="0" align="center" name="jezdziectwo" placeholder="Jeździectwo"></li>
-		<li><input class="pass" type="number" min="0" align="center" name="opatrywanie" placeholder="Opatrywanie ran"></li>
+		<p>Związane z inteligencją: <div id="pktUmiejInt"></div></p>
+		<ul class="lista" onchange="max10int()" style="list-style-type:none;">
+		<li><input class="pass" type="number" min="0" align="center" name="taktyka" placeholder="Taktyka" id="ta"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="nawigacja" placeholder="Nawigacja" id="na"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="inzynieria" placeholder="Inżynieria" id="in"></li>
 		</ul>
 
 		<br>
-		<p>Związane z inteligencją:</p>
-		<ul style="list-style-type:none;">
-		<li><input class="pass" type="number" min="0" align="center" name="taktyka" placeholder="Taktyka"></li>
-		<li><input class="pass" type="number" min="0" align="center" name="nawigacja" placeholder="Nawigacja"></li>
-		<li><input class="pass" type="number" min="0" align="center" name="inzynieria" placeholder="Inżynieria"></li>
+		<p>Związane z charyzmą: <div id="pktUmiejCha"></div></p>
+		<ul class="lista" onchange="max10cha()" style="list-style-type:none;">
+		<li><input class="pass" type="number" min="0" align="center" name="perswazja" placeholder="Perswazja" id="pe"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="przywodztwo" placeholder="Przywództwo" id="pr"></li>
+		<li><input class="pass" type="number" min="0" align="center" name="handel" placeholder="Handel" id="ha"></li>
 		</ul>
-
-		<br>
-		<p>Związane z charyzmą:</p>
-		<ul style="list-style-type:none;">
-		<li><input class="pass" type="number" min="0" align="center" name="perswazja" placeholder="Perswazja"></li>
-		<li><input class="pass" type="number" min="0" align="center" name="przywodztwo" placeholder="Przywództwo"></li>
-		<li><input class="pass" type="number" min="0" align="center" name="handel" placeholder="Handel"></li>
-		</ul>
+		
+		
 </section>
 	<input type="submit" value="Utwórz Postać" name="rejestruj" style="margin-top: 8%;" class="submit" align="center">
 	</form>
