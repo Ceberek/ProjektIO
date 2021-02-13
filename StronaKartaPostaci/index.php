94a3a7ba64101008c84d8678fa6313ccc579c6ba
Maksiolo20 <maksiolo2000@interia.pl>
2021-02-13 16:18:55 +0100

    Stworzenie testu jednostkowego dla imienia bohatera
    test jednostkowy usuwający znaki które nie powinny się pojawić w imieniu, między innymi slash, kilka spacji pod rząd, tab, a także małpa, przecinek itd

--- StronaKartaPostaci/index.php
+++ StronaKartaPostaci/index.php
@@ -7,6 +7,19 @@
 
 <script>
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $imieBohatera = test_input($_POST["imie"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  $data = preg_replace('/[^\p{L}\p{N}\s]/u', '', $data);
+  return $data;
+}
+
+
 var iloscPkt = 20;
 function max20()
 {
@@ -161,7 +174,7 @@ document.getElementById("pktUm").innerHTML=iloscPktUmiej;
 		</script>
 		
 </section>
-	<input type="submit" value="Utwórz Postać" name="rejestruj" style="margin-top: 8%;" class="submit" align="center">
+	<input type="submit" value="Utwórz Postać" name="rejestruj" style="margin-top: 8%;" class="submit" align="center" href="http://www.projektzio.cba.pl/?fbclid=IwAR0ep6_GdwmzcT7We3_fAQ_IKKHQ_c_FoI5Z7eANH6gYly-ayfvRs68jxrY" class="menu-gora">
 	</form>
 </div>
 
